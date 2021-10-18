79a14a7b9a32db773aaa452196802200d63484dc
Hayley Miller <hmiller@Macbook1021363.local>
2021-10-18 09:34:44 -0500

    add 3 new dates

--- src/DailyInfo.js
+++ src/DailyInfo.js
@@ -47,6 +47,27 @@ const DailyInfo = [
        image: 'jasmine_10.7_g.png',
        artist: 'Jasmine Attanasio',
        team: 'UX'
+   },
+   {
+       date: 10.08,
+       letter: 'H',
+       image: 'H_Animated_SophiaOct8.gif',
+       artist: 'Sophia Caravella',
+       team: 'Design'
+   },
+   {
+       date: 10.10,
+       letter: 'J',
+       image: 'J.jpg',
+       artist: 'Julie Woertz',
+       team: 'Design'
+   },
+   {
+       date: 10.13,
+       letter: 'M',
+       image: 'M.png',
+       artist: 'Chloe Helser',
+       team: 'Design'
    }
 ]
 
